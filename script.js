4faba330821e78798f7003faa0bd45b799966641
Bronwen27 <118174027+Bronwen27@users.noreply.github.com>
2023-01-16 22:50:18 +0000

    Password initial prompts and commands written in JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 // Array of special characters to be included in password
 var specialCharacters = [
   '@',
@@ -90,16 +92,100 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+ // Asks user for input 
+  let length = parseInt(
+  prompt("How many Characters would you like in your password? Please choose between 10 and 64")
+ )
+ // Incase user uses any other characters other than numbers  
+ if(isNaN(length)===true){
+  alert(`Please provide password length between 10 and 64 as a number`);
+  return;
+ }
+ // Incase user does not pick correct number of characters.
+ if (length < 10) {
+  alert(`Password must be at least 10 characters`);
+  return;
+ }
+
+ if (length > 64){
+  alert(`Password must be less than 65`);
+  return;
+ }
+
+ let hasSpecialCharacters = confirm (
+  "Click OK to confirm that the password must contain special characters"
+ )
+ let hasNumericCharacters = confirm (
+  "Click OK to confirm that the password must contain numeric characters"
+ )
+ let hasLowerCaseCharacters = confirm (
+  "Click OK to confirm that the password must contain lowercase characters"
+ )
+ let hasUpperCaseCharacters = confirm (
+  "Click OK to confirm that the password must contain uppercase characters"
+ )
+
+if(hasSpecialCharacters === false &&
+  hasNumericCharacters === false &&
+  hasLowerCaseCharacters === false &&
+  hasUpperCaseCharacters === false){
+    alert(`Must select at least one character type`);
+    return;
+  }
+
+  let getPasswordOptions ={
+    length:length,
+    hasSpecialCharacters: hasSpecialCharacters,
+    hasNumericCharacters: hasNumericCharacters,
+    hasLowerCaseCharacters: hasLowerCaseCharacters,
+    hasUpperCaseCharacters: hasUpperCaseCharacters
+  }
+console.log(getPasswordOptions);
+return getPasswordOptions;
 
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
+ let randomIndex = Math.floor(Math.random() * arr.length)
+ let randomElement = arr[randomIndex];
+ return randomElement;
 
 }
 
 // Function to generate password with user input
 function generatePassword() {
+ let options = getPasswordOptions();
+ console.log(options);
+ let result = []
+ let possibleCharacters = []
+ let guaranteedCharacters = []
+
+
+ if(options["hasLowerCaseCharacters"]) {
+  possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+  guaranteedCharacters.push(getRandom(lowerCasedCharacters))
+ }
+ if (options["hasLowerUpperCharacters"]) {
+  possibleCharacters = possibleCharacters.concat(UpperCasedCharacters);
+  guaranteedCharacters.push(getRandom(upperCasedCharacters))
+ }
+if (options["hasSpecialCharacters"]) {
+  possibleCharacters = possibleCharacters.concat(specialCharacters);
+  guaranteedCharacters.push(getRandom(specialCharacters))
+ }
+ if (options["hasNumericCharacters"]) {
+  possibleCharacters = possibleCharacters.concat(numericCharacters);
+  guaranteedCharacters.push(getRandom(numericCharacters))
+ }
+
+ for(let i = 0; i < options.length; i++) {
+  possibleCharacters = getRandom(possibleCharacters);
+  result.push(possibleCharacters);
+  console.log.apply(possibleCharacters);
+ }
+
+ console.log(result);
 
 }
 
